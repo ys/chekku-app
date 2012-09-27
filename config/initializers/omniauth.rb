183ecac992b21a225230f4c5f4f0b96fdf79560e
Yannick Schutz <yannick.schutz@gmail.com>
2012-09-27 20:53:42 +0200

    github oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], callback_url: 'http://localhost:3000/callback'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], callback_url: 'http://chekku.co/callback'
 end
